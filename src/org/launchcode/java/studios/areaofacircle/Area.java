f179c0d77349c8b81cabe4593787abdd15cca5cf
Nichelle Barton <52866856+nbarton76@users.noreply.github.com>
2023-05-24 19:54:10 -0500

    created

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,18 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] args) {
+        double radius;
+        double area;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the radius ");
+        radius = input.nextDouble();
+        input.close();
+        area = Math.PI * radius * radius;
+        System.out.println("The area of a circle of radius:" + area);
+
+    }
+
+}
