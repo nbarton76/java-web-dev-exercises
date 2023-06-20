d245b9c30486c6927d9f8edb32a0ed22cc613e73
Nichelle Barton <52866856+nbarton76@users.noreply.github.com>
2023-06-19 21:46:10 -0500

    Update

---
+++ src/org/launchcode/java/studio/restaurant/Categories.java
@@ -0,0 +1,7 @@
+package org.launchcode.java.studio.restaurant;
+
+public enum Categories {
+    Dinner,
+    Main,
+    Desert
+}
--- src/org/launchcode/java/studio/restaurant/MenuItem.java
+++ src/org/launchcode/java/studio/restaurant/MenuItem.java
@@ -1,70 +1,112 @@
 package org.launchcode.java.studio.restaurant;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Currency;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class MenuItem {
     private String name;
-    private String category;
+    private double price;
     private String description;
-    public Date dateAdded;
-    private Double price;
-    private boolean isNew;
+    private Enum category;
+    private boolean newItem = false;
+    private Date updated;
 
-    public MenuItem(String name, String category, String description, Date dateAdded, Double price){
+    public MenuItem(double price, String description, Enum category, boolean newItem, Date updated, String name) {
         this.name = name;
-        this.category = category;
-        this.description = description;
-        this.dateAdded = dateAdded;
         this.price = price;
-
+        this.description = description;
+        this.category = category;
+        this.newItem = newItem;
+        this.updated = updated;
     }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Enum getCategory() {
+        return category;
+    }
+
+    public void setCategory(Enum category) {
+        this.category = category;
+    }
+
+    public boolean isNewItem() {
+        return newItem;
+    }
+
+    public void setNewItem(boolean newItem) {
+        this.newItem = newItem;
+    }
+
     public String getName() {
-        return this.name;
-    }
-    public String getCategory(){
-        return this.category;
-    }
-    public String getDescription(){
-        return this.description;
-    }
-    public Date getDateAdded(){
-        return this.dateAdded;
-    }
-    public Double getPrice(){
-        return this.price;
-    }
-    public boolean getIsNew(){
-        return this.isNew;
+        return name;
     }
 
-    public void setName(String aName){
-        this.name = aName;
-    }
-    public void setCategory(String aCategory){
-        this.category = aCategory;
-    }
-    public void setDescription(String aDescription){
-        this.description = aDescription;
-    }
-    public void localDate  dateAdded (Date aDateAdded){
-        this.dateAdded = aDateAdded;
-    }
-    public void setPrice(Double aPrice){
-        this.price = aPrice;
-    }
-    public void setIsNew(boolean aIsNew){
-        this.isNew = aIsNew;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof MenuItem)) return false;
-        MenuItem menuItem = (MenuItem) o;
-        return isNew == menuItem.isNew && getName().equals(menuItem.getName()) && getCategory().equals(menuItem.getCategory()) && getDescription().equals(menuItem.getDescription()) && getDateAdded().equals(menuItem.getDateAdded()) && getPrice().equals(menuItem.getPrice());
+    public String toString() {
+        return name + "\n" + (isNewItem() ? "new!!" + "\n" : "")  +  "price: " + price + "\n" + "desc: " + description  + "\n" + "category: " + category;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getName(), getCategory(), getDescription(), getDateAdded(), getPrice(), isNew);
+    public void determineNew() {
+        Date today = new Date();
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(today);
+        cal.add(Calendar.DAY_OF_MONTH, -30);
+        Date today30 = cal.getTime();
+
+        if (this.updated.after(today30)) {
+            this.newItem = true;
+        } else {
+            this. newItem = false;
+        }
     }
+
+    public boolean equals(MenuItem comparison) {
+        if (comparison == this) {
+            return true;
+        }
+
+        if (comparison == null) {
+            return false;
+        }
+
+        if (comparison.getClass() != getClass()) {
+            return false;
+        }
+
+        MenuItem theItem = (MenuItem) comparison;
+        return theItem.getDescription() == getDescription();
+    }
+
+}
+
+
+//        MenuItem
+//        properties-
+//        - dateAdded: Date
+//        - isNew: Boolean
+//        - price: Currency
+//        - description: String
+//        - name: String
+//        - category: String
+//         methods-
+//        - determineNew: (Date), Boolean
 }
