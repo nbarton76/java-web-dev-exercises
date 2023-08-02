120ff8fc529bb20f8ac9f1d1f6c609a6a42984ec
Nichelle Barton <52866856+nbarton76@users.noreply.github.com>
2023-08-02 17:15:07 -0500

    Updates

--- temperature-converter/temperature-converter/src/main/java/org/launchcode/temperatureconverter/TemperatureConverterApplication.java
+++ temperature-converter/temperature-converter/src/main/java/org/launchcode/temperatureconverter/TemperatureConverterApplication.java
@@ -1,9 +1,13 @@
 package org.launchcode.temperatureconverter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+
 public class TemperatureConverterApplication {
 
 	public static void main(String[] args) {
